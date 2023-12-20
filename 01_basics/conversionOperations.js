6a4df2fa3cb3a67adb5bcf10979eb0924884a011
Aman Chandrakar <60728144+AmanChandrakar03@users.noreply.github.com>
2023-12-20 07:05:39 +0000

    Changes made done

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -3,18 +3,28 @@ console.log(typeof num);
 console.log(num);
 
 let numberInValue = Number(num);
-console.log(typeof numberInValue);
-console.log(numberInValue);
+// console.log(typeof numberInValue);
+// console.log(numberInValue);
 
 //"33" -> 33
 //"33abc" -> NaN
 
 let n = "";
 let numIsBool = Boolean(n);
-console.log(numIsBool);
-console.log(typeof(numIsBool));
+// console.log(numIsBool);
+// console.log(typeof(numIsBool));
 
 //1 = true, 0 = false
 
 //"" = false
 //"aman" = true
+
+//console.log(2-2);
+
+let g = f = s = 2+2;
+console.log(s);
+
+console.log("@"+2);
+console.log(" "+2);
+console.log(2+2+"aman"+2);
+
