7678955f40cab46fadf47d305947a7453610e4b6
Aman Chandrakar <60728144+AmanChandrakar03@users.noreply.github.com>
2023-12-20 07:46:37 +0000

    summary of all datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,15 @@
+//Primitive
+// Number, Boolean, String, null, Undefined, Symbol, BigInt
+
+//Non Primitive
+//Arrays, Objects, Functions
+const heros = ["Batman","Superman","Aquaman","A-man"];
+let myObj = {
+    name: "Aman",
+    age : 23,
+     rank:"Associate Engineer",
+     sports: "table Tennis"
+}
+const myFunction = function(){
+    console.log("Hello World");
+}
