2c72512d393ae181886ead75f48c37cea33da443
Aman Chandrakar <60728144+AmanChandrakar03@users.noreply.github.com>
2023-12-21 05:27:43 +0000

    Date

---
+++ 01_basics/date.js
@@ -0,0 +1,9 @@
+const myDate = new Date();
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toString());
+console.log(myDate.toJSON());
+console.log(myDate.getDate());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+
